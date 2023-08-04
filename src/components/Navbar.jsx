8e2d3e816271a77632c514c084fd95315b695617
Anubhav Gupta <2005853@kiit.ac.in>
2023-08-04 11:23:52 +0530

    Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import {motion} from "framer-motion"
 export default function Navbar() {
   return (
     <div className="navbar-container">
-      <div className="navbar-container-left">
-        <Link href="/"><Image height={45} width={100} src="/logo.png " alt="logo" /></Link>
-      </div>
+      <motion.div className="navbar-container-left"
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}>
+        <Link href="/"><Image height={70} width={70} src="/../public/logo.png " alt="logo" /></Link>
+      </motion.div>
       <div className="navbar-container-right">
         <div><Link href="/examples" style={{
           textDecoration:"none",
